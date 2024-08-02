aa94409ebba42c4feee5cba089f976657c5889fc
Alex Scott <alex@alexscott.net>
2024-08-02 17:01:54 +0200

    Working to make this work with reading data.

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,8 @@ import qualified Data.ByteString.Lazy as B
 import Data.Aeson (encode, object, (.=), Value)
 import Data.Aeson.Key (fromString)
 import Data.Aeson.KeyMap (KeyMap)
+-- import Debug.Trace
+import Text.Read (readMaybe)
 
 data Choice = Choice {
     choiceText :: String,
@@ -37,21 +39,29 @@ parseCSV = parse csvFile "(unknown)"
 
 convertToStory :: [[String]] -> Story
 convertToStory rows = foldr addRow M.empty (drop 1 rows)
-  where
-    addRow :: [String] -> Story -> Story
-    addRow [partStr, text, choiceText, choiceSvg, nextPartStr] story =
-        let part = read partStr
-            nextPart = read nextPartStr
-            choice = Choice choiceText choiceSvg nextPart
-        in M.insertWith addChoice part (Part text [choice]) story
-    addRow [partStr, text, _, _, _] story
-      | not (null partStr) && not (null text) =
-        let part = read partStr
-        in M.insertWith (const id) part (Part text []) story
-    addRow _ story = story  -- Skip any malformed rows
 
-    addChoice :: Part -> Part -> Part
-    addChoice (Part t1 c1) (Part _ c2) = Part t1 (c1 ++ c2)
+addTry :: [String] -> M.Map Int String
+addTry [] = error "Missing data"
+addTry (x:xs) = case readMaybe x :: Maybe Int of
+    Nothing -> error "Invalid key format"
+    Just id ->
+        if concat (tail xs) == []
+        then M.insert id (head xs) M.empty -- Items after the second item are empty!
+        else M.insert id ((head xs) ++ "stuff to come") M.empty
+  
+addRow :: [String] -> Story -> Story
+addRow [partStr, text, choiceText, choiceSvg, nextPartStr] story =
+  let part = read partStr
+      nextPart = read nextPartStr
+      choice = Choice choiceText choiceSvg nextPart
+  in M.insertWith addChoice part (Part text [choice]) story
+    -- addRow [partStr, text, _, _, _] story =
+    --     let part = read partStr
+    --     in M.insertWith addChoice part (Part text []) story
+    -- addRow _ story = story  -- Skip any malformed rows
+
+addChoice :: Part -> Part -> Part
+addChoice (Part t1 c1) (Part _ c2) = Part t1 (c1 ++ c2)
 
 storyToJSON :: Story -> Value
 storyToJSON story = object $ map partToJSON (M.toList story)
@@ -71,3 +81,5 @@ main = do
             let story = convertToStory rows
             B.writeFile "story.json" (encode $ storyToJSON story)
             putStrLn "story.json generated successfully."
+
+
