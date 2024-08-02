cb6a24e276d9dc12408098a40b92ac8aa5ba10b0
Alex Scott <alex@alexscott.net>
2024-08-02 17:59:43 +0200

    Little cleanup.

--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,6 @@ import qualified Data.ByteString.Lazy as B
 import Data.Aeson (encode, object, (.=), Value)
 import Data.Aeson.Key (fromString)
 import Data.Aeson.KeyMap (KeyMap)
--- import Debug.Trace
 import Text.Read (readMaybe)
 
 data Choice = Choice {
@@ -38,35 +37,24 @@ parseCSV :: String -> Either ParseError [[String]]
 parseCSV = parse csvFile "(unknown)"
 
 convertToStory :: [[String]] -> Story
-convertToStory rows = foldr addRow M.empty (drop 1 rows)
+convertToStory rows = foldr addLine M.empty (drop 1 rows)
 
-l1 = ["1","Some text","", "", ""]
-l2 = ["1","Some text and more","A", "B", "C"]
+-- Test data.
+-- l1 = ["1","Some text","", "", ""]
+-- l2 = ["1","Some text and more","A", "B", "C"]
 
-addTry :: [String] -> M.Map Int String
-addTry [] = error "Missing data"
-addTry (x:text:xs) = case readMaybe x :: Maybe Int of
+addLine :: [String] -> Story -> Story
+addLine (x:text:xs) story = case readMaybe x :: Maybe Int of
     Nothing -> error "Invalid key format"
     Just id ->
         if concat xs == []
-        then M.insert id text M.empty -- Items after the second item are empty!
-        else M.insertWith addChoice id (Part text [choice]) M.empty
+        then M.insert id (Part text []) story -- At end, so empty no choices.
+        else M.insertWith addChoice id (Part text [choice]) story
         where choiceText = head xs
               choiceSvg = head (tail xs)
               nextPart = readMaybe (head (tail (tail xs))) :: Maybe Int
               choice = Choice choiceText choiceSvg nextPart
   
-addRow :: [String] -> Story -> Story
-addRow [partStr, text, choiceText, choiceSvg, nextPartStr] story =
-  let part = read partStr
-      nextPart = read nextPartStr
-      choice = Choice choiceText choiceSvg nextPart
-  in M.insertWith addChoice part (Part text [choice]) story
-    -- addRow [partStr, text, _, _, _] story =
-    --     let part = read partStr
-    --     in M.insertWith addChoice part (Part text []) story
-    -- addRow _ story = story  -- Skip any malformed rows
-
 addChoice :: Part -> Part -> Part
 addChoice (Part t1 c1) (Part _ c2) = Part t1 (c1 ++ c2)
 
